ffa99b972062a0fcab13ebd97bc11ab76a06ab03
Karri Lakshmi Sravanthi <84621897+20P31A04E2@users.noreply.github.com>
2023-03-28 19:11:53 +0530

    Add files via upload

---
+++ strRev.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class strRev {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here\. Print output to STDOUT. */
+        int b=A.length();
+        String u="";
+        for(int i=b-1;i>=0;i--)
+        {
+            u=u+A.charAt(i);
+        }
+    
+       boolean t;
+       t=A.equals(u);
+       
+        
+        if(t==true)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+        sc.close();
+    }
+}
+
+
